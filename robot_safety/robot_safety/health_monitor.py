5a897f5c3ef1a5ec611f61851dba1b41538175d2
hibabhll <bouhlelhiba38@gmail.com>
2026-09-24 23:12:10 +0100

    Update health_monitor.py
    Removed outdated comments to enhance code clarity.

--- robot_safety/robot_safety/health_monitor.py
+++ robot_safety/robot_safety/health_monitor.py
@@ -7,12 +7,12 @@ class HealthMonitor(Node):
     def __init__(self):
         super().__init__('health_monitor')
         
-        # 1. L'ODHNIN W L'FOMM (kima lbere7)
+       
         self.subscription = self.create_subscription(
             SensorData, 'sensor_data', self.listener_callback, 10)
         self.publisher_ = self.create_publisher(String, 'robot_status', 10)
 
-        # 2. 💎 L'PARAMETERS: N3arfou l ROS2 enna 3andna 4 paramètres, w l'défaut mte3hom hakka:
+       
         self.declare_parameter('temp_warn', 60.0)
         self.declare_parameter('temp_crit', 75.0)
         self.declare_parameter('batt_warn', 30.0)
@@ -21,13 +21,13 @@ class HealthMonitor(Node):
         self.get_logger().info('Health Monitor demarre (b Parameters)!')
 
     def listener_callback(self, msg):
-        # 3. N9RAW L'PARAMÈTRES (kol marra yji message, bash ken badlnahom LIVE, ya9rahom dghar)
+
         temp_warn = self.get_parameter('temp_warn').value
         temp_crit = self.get_parameter('temp_crit').value
         batt_warn = self.get_parameter('batt_warn').value
         batt_crit = self.get_parameter('batt_crit').value
 
-        # 4. L'MACHINE À ÉTATS (tawa b l'variables, machi b l'ar9am msamra!)
+  
         if msg.motor_temp >= temp_crit or msg.battery_level <= batt_crit:
             status = 'CRITICAL'
         elif msg.motor_temp >= temp_warn or msg.battery_level <= batt_warn:
