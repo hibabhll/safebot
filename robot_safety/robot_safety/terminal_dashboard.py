e47807e95f6c84c6ac2b668d2e3385a16af72464
hibabhll <bouhlelhiba38@gmail.com>
2026-09-24 23:13:25 +0100

    Update terminal_dashboard.py
    Removed commented lines in Arabic and improved code readability.

--- robot_safety/robot_safety/terminal_dashboard.py
+++ robot_safety/robot_safety/terminal_dashboard.py
@@ -3,7 +3,6 @@ from rclpy.node import Node
 from robot_safety_interfaces.msg import SensorData
 from std_msgs.msg import String
 
-# Importations mte3 l'makeup (Rich)
 from rich.console import Console
 from rich.table import Table
 
@@ -18,15 +17,14 @@ class TerminalDashboard(Node):
         self.cpu = 50.0
         self.status = "NORMAL"
 
-        # 2. Les Subscribers (Yfadhw l'mémoire)
+  
         self.sub_sensor = self.create_subscription(SensorData, 'sensor_data', self.sensor_cb, 10)
         self.sub_status = self.create_subscription(String, 'robot_status', self.status_cb, 10)
         
-        # 3. Timer l'affichage (Kol 0.5 seconde, yrsem l'écran)
         self.timer = self.create_timer(0.5, self.draw_screen)
 
     def sensor_cb(self, msg):
-        # Nfadhw l'mémoire bla ma nrsmou
+    
         self.battery = msg.battery_level
         self.temp = msg.motor_temp
         self.cpu = msg.cpu_load
@@ -35,24 +33,24 @@ class TerminalDashboard(Node):
         self.status = msg.data
 
     def draw_screen(self):
-        # Création mte3 l'tableau
+        
         table = Table(title=" ROBOT SAFETY DASHBOARD ", show_header=True, header_style="bold magenta")
         table.add_column("Mesure", style="cyan", no_wrap=True)
         table.add_column("Valeur", justify="right")
         table.add_column("État", justify="center")
 
-        # Logique des couleurs (L'intelligence mte3 l'dashboard)
+    
         bat_style = "green" if self.battery > 30 else "bold red"
         temp_style = "green" if self.temp < 60 else "bold red"
         status_style = "green" if self.status == "NORMAL" else "bold red"
 
-        # Remplissage
+  
         table.add_row(" Batterie", f"{self.battery:.1f} %", f"[{bat_style}]{self.battery:.0f}%[/{bat_style}]")
         table.add_row(" Température", f"{self.temp:.1f} °C", f"[{temp_style}]{self.temp:.0f}°C[/{temp_style}]")
         table.add_row(" CPU", f"{self.cpu:.1f} %", f"{self.cpu:.0f}%")
         table.add_row(" Status", self.status, f"[{status_style}]{self.status}[/{status_style}]")
 
-        # L'SI7R: Nmsa7 l'écran w naffich l'tableau f nafs l'blasa
+       
         self.console.clear()
         self.console.print(table)
 
